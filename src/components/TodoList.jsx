2f9b5fc1049d6a5ca138c988ee0ea2d00103de67
rayzurr <137191611+rayzurr@users.noreply.github.com>
2024-05-25 11:05:33 +1000

    first commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React from 'react'
+import TodoCard from './TodoCard'
 
-export default function TodoList() {
-  let todos = ["Go to the gym", "Eat more fruits", "Pick up kids from school"];
+export default function TodoList(props) {
+
+    const { todos } = props
+    
   return (
-    <ul className="main">
-      {todos.map((todo, todoIndex) => {
-        return (
-          <TodoCard key={todoIndex}></TodoCard>
-        );
-      })}
+    <ul className='main'>
+         {todos.map((todo, todoIndex) => {
+            return (
+                <TodoCard {...props} key={todoIndex} index={todoIndex}>
+                    <p>{todo}</p>
+                </TodoCard>
+            )
+         })}
     </ul>
-  );
+  )
 }
